498017e0e5c83ab019f7e48d6e3796f4f72276d5
withsukrann7 <sukran1903yilmaz@gmail.com>
2025-08-28 02:52:43 +0300

    duzenleme

--- metallicgarage/lib/firebase-admin.ts
+++ metallicgarage/lib/firebase-admin.ts
@@ -1,15 +1,36 @@
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
-import { getDatabase } from 'firebase-admin/database';
+
+import { cert, getApps, initializeApp, AppOptions } from "firebase-admin/app";
+import { getDatabase } from "firebase-admin/database";
+
+type ServiceAccount = {
+  type: string;
+  project_id: string;
+  private_key_id: string;
+  private_key: string;
+  client_email: string;
+  client_id: string;
+  auth_uri: string;
+  token_uri: string;
+  auth_provider_x509_cert_url: string;
+  client_x509_cert_url: string;
+  universe_domain: string;
+};
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT env yok!");
+}
+
+const serviceAccount: ServiceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+const appOptions: AppOptions = {
+  credential: cert(serviceAccount as any),
+  databaseURL: process.env.FIREBASE_DB_URL,
+};
 
 if (!getApps().length) {
-    initializeApp({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
-        databaseURL: process.env.FIREBASE_DB_URL,
-    });
+  initializeApp(appOptions);
 }
 
 export const db = getDatabase();
